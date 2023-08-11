fcd4723c08b1b26ae6f7edbd76ab896e9d01c250
husam_eldeen <haskuuuuu@gmail.com>
2023-08-10 17:17:49 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", siezof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n," sizof(float));
+	return (0);
+}
+
