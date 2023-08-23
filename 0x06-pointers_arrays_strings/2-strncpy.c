1a8450328094d02852fba9cdf7867a79259679f5
husam_eldeen <haskuuuuu@gmail.com>
2023-08-23 14:51:47 -0700

    betty style

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,25 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++) /** copy characters from src to dest 
-						    until n characters are copied or src ends*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	/**
+	 *copy characters from src to dest
+	 * until n characters are copied or src ends
+	 */
 	{
 	dest[i] = src[i];
 	}
-	for (; i < n; i++) /* if the length of src is less than n, pad the remainder of dest with null bytes */
+	for (; i < n; i++)
+	/**
+	 *if the length of sr
+	 * is less than n, pad the
+	 * remainder of dest with null bytes
+	 */
 	{
 	dest[i] = '\0';
 	}
-	
-	return dest; /* return the destination string */
+
+	return (dest);
+	/* return the destination string */
 }
 
