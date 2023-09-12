8a35b7f04c840eabaada227229ff2ee777df7be6
husam_eldeen <haskuuuuu@gmail.com>
2023-09-12 11:50:04 -0700

    0-object_like_macro.h

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -2,19 +2,13 @@
 
 /**
  * main - Print _putchar
- * Return: Always 0
+ * @c: the charecter to print
+ * return: on success 1
+ * on error, -1 is returned
  */
-int main(void)
+
+int _putchar(char c)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	return (write(1, &c, 1));
 }
 
